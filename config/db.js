883f01371c92e97781a290e2287b90b739787e46
Edddo123 <edo.miraqian@gmail.com>
2021-02-15 01:33:04 +0400

    make it better

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
+  connectionString:process.env.DATABASE_URL,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.HOST,
