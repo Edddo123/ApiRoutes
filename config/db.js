117d260084c366aca80ef02b347baa76a69f5e72
Edddo123 <edo.miraqian@gmail.com>
2021-02-21 16:53:16 +0400

    db for production and dev

--- config/db.js
+++ config/db.js
@@ -1,10 +1,23 @@
 const { Pool } = require("pg");
+let pool;
 
-const pool = new Pool({
-  ssl: { rejectUnauthorized: false },
-  connectionString:process.env.DATABASE_URL,
-  max: 10,
-  connectionTimeoutMillis: 0,
-});
+if (process.env.NODE_ENV === "production") {
+  pool = new Pool({
+    ssl: { rejectUnauthorized: false },
+    connectionString: process.env.DATABASE_URL,
+    max: 10,
+    connectionTimeoutMillis: 0,
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    max: 10,
+    connectionTimeoutMillis: 0,
+  });
+}
 
 module.exports = pool;
